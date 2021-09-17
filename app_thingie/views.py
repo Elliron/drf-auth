bd087849a69ff5b79ee5c99c01ca310c2767d554
Glenn Elliron Clark <Elliron.Clark@gmail.com>
2021-09-16 23:48:17 -0700

    API tokens are working, still need to setup user and Docker

--- app_thingie/views.py
+++ app_thingie/views.py
@@ -1,11 +1,15 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import App_thingieSerializer
 from .models import App_thingie
+from .permissions import IsOwnerOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 class App_thingieList(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly)
     queryset = App_thingie.objects.all()
     serializer_class = App_thingieSerializer
 
 class App_thingieDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsOwnerOrReadOnly,)
     queryset = App_thingie.objects.all()
     serializer_class = App_thingieSerializer
